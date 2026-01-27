b1ae6ea9252addc0c7891f3606b64f1402fcdefe
ddddd727 <77392831+ddddd727@users.noreply.github.com>
2026-01-27 09:57:47 +0800

    Signed-off-by: ddddd727 <77392831+ddddd727@users.noreply.github.com>

---
+++ Dtos/Dict/DictConfig.cs
@@ -0,0 +1,27 @@
+﻿namespace PMCSystem_Backend.Dtos.Dict
+{
+    // ⬇️ 命名空间改为了 PMCSystem_Backend.Dtos.Dict
+    // 这样只有引用了这个命名空间的地方才能看到它，不会干扰全局
+
+    public class RootDictConfig
+    {
+        public Dictionary<string, string> CommonColumnMapping { get; set; }
+        public Dictionary<string, DictItemConfig> DictConfiguration { get; set; }
+    }
+
+    public class DictItemConfig
+    {
+        public string TableName { get; set; }
+        public List<DictColumnConfig> Columns { get; set; }
+    }
+
+    public class DictColumnConfig
+    {
+        public string DbField { get; set; }
+        public string Title { get; set; }
+        public bool IsHidden { get; set; }
+        public bool IsRequired { get; set; }
+        public string UiType { get; set; }
+        public string DataSource { get; set; }
+    }
+}
---
+++ Dtos/Dict/DictTableDto.cs
@@ -0,0 +1,18 @@
+﻿namespace PMCSystem_Backend.Dtos.Dict
+{
+    public class DictTableDto
+    {
+        public List<DictColumnDto> Columns { get; set; } = new();
+        public IEnumerable<dynamic> Rows { get; set; }
+    }
+
+    public class DictColumnDto
+    {
+        public string Prop { get; set; }
+        public string Label { get; set; }
+        public bool Show { get; set; }
+        public string UiType { get; set; }
+        public bool Required { get; set; }
+        public string DataSource { get; set; }
+    }
+}
