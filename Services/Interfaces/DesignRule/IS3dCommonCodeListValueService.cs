02cbc4088a5aedb779e700fcfb8c49f3027a855f
ddddd727 <77392831+ddddd727@users.noreply.github.com>
2026-03-18 08:37:41 +0800

    修复designrule 文件

--- Services/Interfaces/DesignRule/IS3dCommonCodeListValueService.cs
+++ Services/Interfaces/DesignRule/IS3dCommonCodeListValueService.cs
@@ -1,13 +1,11 @@
 using PMCSystem_Backend.Models;
-// ÐÂÔö
-using PMCSystem_Backend.Dtos.Dict;
 
 namespace PMCSystem_Backend.Services.Interfaces
 {
     public interface IS3dCommonCodeListValueService
     {
 
-        Task<IEnumerable<dynamic>> GetOptionsAsync(string tableName, DataSourceRelationConfig? relation = null);
+        Task<IEnumerable<dynamic>> GetOptionsAsync(string tableName);
         Task<IEnumerable<S3dCommonCodeListValueDto>> GetMaterialsCategoryAsync();
         Task<IEnumerable<S3dCommonCodeListValueDto>> GetScheduleThicknessAsync();
         Task<IEnumerable<S3dCommonCodeListValueDto>> GetEndStandardAsync();
